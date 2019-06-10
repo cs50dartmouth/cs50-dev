743f19624070461d068e98678edae9dc193885bf
David Kotz <David.F.Kotz@dartmouth.edu>
2019-06-10 09:24:06 -0400

    updated and moved the package list into the provision script. They had become disconnected.

--- setup/provision.sh
+++ setup/provision.sh
@@ -25,20 +25,22 @@ function try() {
 echo Logging to $log...
 echo Provision started &> $log
 
-echo Protect your ssh keys...
+echo Protect your ssh keys... | tee -a $log
 try "chmod 600 .ssh/id_rsa .ssh/id_rsa.pub"
 
-echo Set to Eastern timezone...
+echo Set to Eastern timezone... | tee -a $log
 try "cp -f /usr/share/zoneinfo/US/Eastern /etc/localtime"
 
-echo Installing necessary packages...
+echo Installing necessary packages... | tee -a $log
+packages='wget git gcc valgrind autoconf emacs manpages-posix-dev
+          libncurses5-dev libncursesw5-dev'
 try "apt-get update"
-for package in wget git gcc valgrind emacs autoconf;
+for package in $packages;
 do
     try "apt-get install -y $package"
 done
 
-echo Installing dot files.
+echo Installing dot files... | tee -a $log
 dotdir=/home/vagrant/cs50-dev/dotfiles/virtualbox
 for dot in $dotdir/*; do
     dotfile=.${dot##*/}
@@ -50,6 +52,6 @@ done
 echo "Do all your work in ~/cs50-dev/" > /home/vagrant/DO-NO-WORK-HERE
 
 if $success
-then echo "Provision succeeded.";
+then echo "Provision succeeded." | tee -a $log;
 else echo "PROVISION FAILED; log in and review $log for details.";
 fi
